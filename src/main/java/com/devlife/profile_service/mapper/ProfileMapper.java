bad276e973980a9eab49c51f723ac10bb036498a
larutyunov <larutyunov@diasoft.ru>
2023-02-07 14:44:45 +0300

    DEV-155 add tests

--- src/main/java/com/devlife/profile_service/mapper/ProfileMapper.java
+++ src/main/java/com/devlife/profile_service/mapper/ProfileMapper.java
@@ -3,6 +3,8 @@ package com.devlife.profile_service.mapper;
 import com.devlife.profile_service.dto.ProfileDto;
 import com.devlife.profile_service.dto.apiRequestDto.InitProfileReq;
 import com.devlife.profile_service.entity.Profile;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
